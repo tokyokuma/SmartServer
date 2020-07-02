2ded1fa21d608b929df76a4fbe9e094ede0a4806
tokyokuma <falblue1052@gmail.com>
2020-07-02 23:30:53 +0900

    add new file

---
+++ server.py
@@ -0,0 +1,23 @@
+# socket サーバを作成
+
+import socket
+
+# AF = IPv4 という意味
+# TCP/IP の場合は、SOCK_STREAM を使う
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # IPアドレスとポートを指定
+    s.bind(('192.168.0.16', 55555))
+    print("Wait Connection")
+    # 1 接続
+    s.listen(1)
+    # connection するまで待つ
+    while True:
+        # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                # データを受け取る
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('data : {}, addr: {}'.format(data, addr))
